48df918b5bc535708628faae0c66c5c601bcf970
Jon Huber <hubermjonathan@gmail.com>
2018-10-20 19:46:16 -0500

    displayed items on receipt after click

--- client-src/home.js
+++ client-src/home.js
@@ -21,11 +21,28 @@ class ItemPicture extends React.Component {
 }
 
 class Items extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addToReceipt = this.addToReceipt.bind(this);
+  }
+
+  addToReceipt(item) {
+    console.log(receiptItems);
+    for(let i = 0; i < receiptItems.length; i++) {
+      if(receiptItems[i].item.id === item.id) {
+        receiptItems[i].quantity = receiptItems[i].quantity + 1;
+      }
+    }
+    ReactDOM.render(
+      <Receipt items={receiptItems}/>, document.getElementById("receiptContainer")
+    );
+  }
+
   render() {
     let items = [];
     for(let i = 0; i < this.props.items.length; i++) {
       items.push(
-        <div key={"col-"+i} className="col-3">
+        <div key={"col-"+i} className="col-3" onClick={this.addToReceipt.bind(this, this.props.items[i])}>
           <Item key={"item-"+i} name={this.props.items[i].name} image={this.props.items[i].image}/>
         </div>
       );
@@ -65,18 +82,57 @@ class Items extends React.Component {
   }
 }
 
+class ReceiptItem extends React.Component {
+  render() {
+    return (
+      <div className={this.props.darkRow ? "row dark align-items-center" : "row align-items-center"}>
+        <div className="col-2">{this.props.quantity}</div>
+        <div className="col-8">{this.props.item.name}</div>
+        <div className="col-2">{this.props.price}</div>
+      </div>
+    );
+  }
+}
+
+class Receipt extends React.Component {
+  render() {
+    let receiptItemsHtml = [];
+
+    let darkRow = false;
+    for(let i = 0; i < receiptItems.length; i++) {
+      if(receiptItems[i].quantity !== 0) {
+        receiptItemsHtml.push(
+          <ReceiptItem key={"receipt-item-"+i} darkRow={darkRow} quantity={receiptItems[i].quantity} item={receiptItems[i].item}/>
+        );
+        darkRow = darkRow ? false : true;
+      }
+    }
+
+    return (
+      <div className="card">
+        <div className="card-body">
+          {receiptItemsHtml}
+        </div>
+      </div>
+    );
+  }
+}
+
 let dbItems = [
   {
     name: 'test 1',
     image: 'test1',
+    id: '1'
   },
   {
     name: 'test 2',
     image: 'test2',
+    id: '2'
   },
   {
     name: 'test 3',
     image: 'test3',
+    id: '3'
   },
   {
     name: 'test 4',
@@ -156,6 +212,19 @@ let dbItems = [
   }
 ]
 
+let receiptItems = [];
+for(let i = 0; i < dbItems.length; i++) {
+  receiptItems.push(
+    {
+      quantity: 0,
+      item: dbItems[i]
+    }
+  );
+}
+
 ReactDOM.render(
   <Items items={dbItems}/>, document.getElementById('itemsContainer')
+);
+ReactDOM.render(
+  <Receipt items={receiptItems}/>, document.getElementById('receiptContainer')
 );
