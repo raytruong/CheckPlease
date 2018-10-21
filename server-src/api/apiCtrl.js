decd380ba5c0c754bf081bdd44f0b5d443c660ab
asionnn <collin.li0499@gmail.com>
2018-10-21 04:18:29 -0400

    checkout

--- server-src/api/apiCtrl.js
+++ server-src/api/apiCtrl.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import schemaCtrl from '../mongo/models/schema';
 
+
 function connectToDb() {
   mongoose.connect("mongodb://admin:admin@cluster0-shard-00-00-ipvsp.gcp.mongodb.net:27017,cluster0-shard-00-01-ipvsp.gcp.mongodb.net:27017,cluster0-shard-00-02-ipvsp.gcp.mongodb.net:27017/wcg?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", { useNewUrlParser: true });
   let db = mongoose.connection;
@@ -8,15 +9,15 @@ function connectToDb() {
   return db;
 }
 
-let getStoreItems = function getStoreItems(req,res){
+let getStoreItems = function getStoreItems(req, res) {
   //return list of items from database
   let db = connectToDb();
   db.once('open', () => {
-    schemaCtrl.item.find({},function(err,items){
-      if(err){
-        res.status(500).send({message: "Error getting items"});
+    schemaCtrl.item.find({}, function (err, items) {
+      if (err) {
+        res.status(500).send({ message: "Error getting items" });
       }
-      else{
+      else {
         res.send(items);
       }
     });
@@ -34,58 +35,68 @@ let addStoreItem = function addStoreItem(req, res) {
     });
 
     newItem.save(function (err, newItem) {
-      if (err) return res.status(500).send({message: 'could not add new item'});
-      else res.status(200).send({message: 'successfully added item to the database'});
+      if (err) return res.status(500).send({ message: 'could not add new item' });
+      else res.status(200).send({ message: 'successfully added item to the database' });
     });
   });
 }
 
-let getLogin = function getLogin(req,res){
+let getLogin = function getLogin(req, res) {
   //return login page;
   return {};
 }
 
-let deleteItem = function deleteItem(req,res){
+let deleteItem = function deleteItem(req, res) {
   //delete an item from the database
   let i = req.params.id;
   let arr = req.body.array;
-  for(let x = 0; x < arr.length;x++){
-    schemaCtrl.item.remove({_id:`${arr[x]._id}`});
+  for (let x = 0; x < arr.length; x++) {
+    schemaCtrl.item.remove({ _id: `${arr[x]._id}` });
   }
 }
 
-let checkout = function checkout(req,res){
+let checkout = function checkout(req, res) {
   //checkout with the items
   let arr = req.body.array;
-  let array = [];
-  for(let x = 0; x < arr.length;x++){
-    let item = {price: arr[x].price, quantity: arr[x].quantity};
-    array.push(item);
-  }
-  res.send(array);
+  let d = new Date();
+
+  db.once('open', () => {
+    let newTransaction = new schemaCtrl.transaction({
+      items: arr.item,
+      date: d,
+      tax: arr.tax,
+      subtotal: arr.subtotal,
+      total: arr.total
+    });
+
+    newItem.save(function (err, newTransaction) {
+      if (err) return res.status(500).send({ message: 'could not proceed to transaction' });
+      else res.status(200).send({ message: 'successfully checked out' });
+    });
+  });
 }
 
-let login = function login(req,res){
+let login = function login(req, res) {
   //check details and authenticate
   return {};
 }
 
-let getManage = function getManage(req,res){
+let getManage = function getManage(req, res) {
   //return manage page
   return {};
 }
 
-let editItem = function editItem(req,res){
+let editItem = function editItem(req, res) {
   //edit item details
   return {};
 }
 
-let getTransactionHistory = function getTransactionHistory(req,res){
+let getTransactionHistory = function getTransactionHistory(req, res) {
   //return all past transactions from database
   return {};
 }
 
-let deleteTransaction = function deleteTransaction(req,res){
+let deleteTransaction = function deleteTransaction(req, res) {
   //delete specified transaction from database
   return {};
 }
